295f8be3be158ab29bb67a8fb77fb39ef5d97360
Fadhilla Alfajr <alfajrfadhilla@gmail.com>
2022-06-26 00:37:34 +0700

    profile

--- frontend/go-scholarship/src/App.js
+++ frontend/go-scholarship/src/App.js
@@ -8,6 +8,8 @@ import Faq from './Pages/Faq';
 import Contct from './Pages/ContactUs';
 import Detailbea from './Pages/Detailbea'
 import Listbea from './Pages/Listbea'
+import Profile from './Pages/Profile'
+
 function App() {
   return (
     <div className="App">
@@ -21,6 +23,7 @@ function App() {
       <Route path="/faq" element={<Faq />}/>
       <Route path="/contact" element={<Contct />}/>
       <Route path="/list-beasiswa" element={<Listbea />}/>
+      <Route path="/profile" element={<Profile />}/>
     </Routes>
     </div>
   );
---
+++ frontend/go-scholarship/src/Pages/Profile.jsx
@@ -0,0 +1,108 @@
+import {
+    Heading,
+    Avatar,
+    Box,
+    Center,
+    Stack,
+    VStack,
+    Button,
+    useColorModeValue,
+    Stat,
+    StatLabel,
+    StatHelpText,
+  } from '@chakra-ui/react';
+  
+  export default function SocialProfileSimple() {
+    return (
+      <Center py={6}>
+        <Box
+          maxW={'320px'}
+          w={'full'}
+          bg={useColorModeValue('white', 'gray.900')}
+          boxShadow={'2xl'}
+          rounded={'lg'}
+          p={6}
+          textAlign={'center'}>
+            <Heading lineHeight={1.1} fontSize={{ base: '2xl', sm: '3xl' }}>
+           Profile
+          </Heading>
+          <Avatar
+            size={'xl'}
+            src={''}
+            alt={'Avatar Alt'}
+            mb={4}
+            pos={'relative'}          
+          />
+          <VStack align={'left'} justify={'left'}  direction={'row'} mt={6}>
+          <Stat>
+          <StatLabel>Nama Lengkap</StatLabel>
+          <StatHelpText>Admin</StatHelpText>
+          </Stat>
+          <Stat>
+          <StatLabel>Pendidikan</StatLabel>
+          <StatHelpText>Mahasiswa</StatHelpText>
+          </Stat>
+          <Stat>
+          <StatLabel>Email</StatLabel>
+          <StatHelpText>admin@gmail.com</StatHelpText>
+          </Stat>
+          </VStack>
+          <center mt={8} >
+          <Button
+              flex={1}
+              fontSize={'sm'}
+              rounded={'full'}
+              bg={'pink.400'}
+              color={'white'}
+              boxShadow={
+                '0px 1px 25px -5px rgb(66 153 225 / 48%), 0 10px 10px -5px rgb(66 153 225 / 43%)'
+              }
+              _hover={{
+                bg: 'pink.400',
+              }}
+              _focus={{
+                bg: 'pink.400',
+              }}>
+              Postingan Saya
+            </Button>
+            </center>
+          <Stack mt={8} direction={'row'} spacing={4}>
+          <Button
+              flex={1}
+              fontSize={'sm'}
+              rounded={'full'}
+              bg={'pink.400'}
+              color={'white'}
+              boxShadow={
+                '0px 1px 25px -5px rgb(66 153 225 / 48%), 0 10px 10px -5px rgb(66 153 225 / 43%)'
+              }
+              _hover={{
+                bg: 'pink.400',
+              }}
+              _focus={{
+                bg: 'pink.400',
+              }}>
+              Hapus Akun
+            </Button>
+            <Button
+              flex={1}
+              fontSize={'sm'}
+              rounded={'full'}
+              bg={'pink.400'}
+              color={'white'}
+              boxShadow={
+                '0px 1px 25px -5px rgb(66 153 225 / 48%), 0 10px 10px -5px rgb(66 153 225 / 43%)'
+              }
+              _hover={{
+                bg: 'pink.400',
+              }}
+              _focus={{
+                bg: 'pink.400',
+              }}>
+              Keluar
+            </Button>
+          </Stack>
+        </Box>
+      </Center>
+    );
+  }
