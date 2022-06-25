81f41e946f74a1e429136be008088ac01d78ca17
yati <hayatin1109@gmail.com>
2022-06-25 22:27:03 +0700

    aboutus

--- frontend/go-scholarship/src/Components/Hero.jsx
+++ frontend/go-scholarship/src/Components/Hero.jsx
@@ -20,23 +20,6 @@ import { SimpleGrid } from "@chakra-ui/react";
 import { IoAnalyticsSharp, IoLogoBitcoin, IoSearchSharp } from "react-icons/io5";
 import { ReactElement } from "react";
 
-interface FeatureProps {
-  text: string;
-  iconBg: string;
-  icon?: ReactElement;
-}
-
-const Feature = ({ text, icon, iconBg }: FeatureProps) => {
-  return (
-    <Stack direction={"row"} align={"center"}>
-      <Flex w={8} h={8} align={"center"} justify={"center"} rounded={"full"} bg={iconBg}>
-        {icon}
-      </Flex>
-      <Text fontWeight={600}>{text}</Text>
-    </Stack>
-  );
-};
-
 function SplitWithImage() {
   return (
     <Container maxW={"full"} py={12} bg={useColorModeValue("pink.50", "pink.800")} mt={0} centerContent overflow="hidden">
